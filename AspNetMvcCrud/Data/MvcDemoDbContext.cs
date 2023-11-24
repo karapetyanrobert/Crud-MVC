7947b191f1a0977d1bde3369be73c0f0ce51cef0
user <karapetyanrobert0223@gmail.com>
2023-11-24 22:18:25 +0400

    Add project files.

---
+++ AspNetMvcCrud/Data/MvcDemoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AspNetMvcCrud.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetMvcCrud.Data
+{
+    public class MvcDemoDbContext : DbContext
+    {
+        public MvcDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
